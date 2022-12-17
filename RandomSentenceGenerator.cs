abbe5dc11e6826c241f4978fac22a7a46ed5d900
dochkoff <hounds.07cinder@icloud.com>
2022-12-17 19:44:03 +0200

    v 2.0
    Implemented logic for declarative and interrogative sentences.

--- RandomSentenceGenerator.cs
+++ RandomSentenceGenerator.cs
@@ -10,26 +10,48 @@ namespace RandomSentenceGenerator
             string[] plaeces = { "Sofia", "Plovdiv", "Varna", "Burgas", "Stara Zagora", "Veliko Tarnovo", "Yambol", "Haskovo", "Balchik", "Blagoevgrad", "Ruse", "Vidin", "Karlovo", "Kazanluk", "Botevgrad", "Pazardzhik", "Asenovgrad", "Svishtov", "Gorna Oryahovitsa", "Pleven", "Sevlievo", "Gabrovo", "Tryavna", "Troyan", "Elena", "Aitos" };
             string[] verbs = { "eats", "holds", "sees", "plays with", "brings", "builds", "burns", "catches", "draws", "dreams", "finds", "forgets", "freezes", "hides", "keeps", "loses", "puts", "sends", "shows", "takes" };
             string[] nouns = { "stones", "cake", "apple", "laptop", "bikes", "pizza", "car", "fruits", "musical instruments", "animal", "baby", "bridge", "camera", "credit card", "computer", "dad", "facts", "fish", "girl", "glass", "grass", "hand", "head" };
-            string[] adverbs = { "slowly", "diligently", "warmly", "sadly", "rapidly", "abnormally", "accidentally", "actually", "almost", "always", "beautifully", "carefully", "closely", "correctly", "easily", "elegantly", "eventually", "exactly", "fast", "often", "only", "safely" };
+            string[] adverbs = { "slowly", "diligently", "warmly", "sadly", "rapidly", "abnormally", "accidentally", "actually", "almost", "always", "beautifully", "carefully", "closely", "correctly", "easily", "elegantly", "eventually", "exactly", "fast", "often", "only for", "safely" };
             string[] detaills = { "near the river", "at home", "in the park", "at work", "near the city", "near the village", "in front of the house", "in the basement", "on the roof", "in the building", "after everything that happened" };
+
+            string[] questionWords = { "Who", "What", "Where", "When", "How" };
+            string[] helpingVerbs = { "does", "has" };
+            string[] questionVerbs = { "eat", "hold", "see", "play with", "bring", "build", "burns", "catche", "draw", "dream", "find", "forget", "freez", "hide", "keep", "lose", "put", "send", "show", "take" };
 
 
             Console.WriteLine("Hello, this is your firs random-generated sentance:");
             while (true)
             {
+                Random random = new Random();
+                int randomNum = random.Next(1, 10);
+
                 string randomName = GetRandomWord(names);
-                string randomPlaece = GetRandomWord(plaeces);
-                string randomVerb = GetRandomWord(verbs);
-                string randomNoun = GetRandomWord(nouns);
                 string randomAdvedb = GetRandomWord(adverbs);
                 string randomDetail = GetRandomWord(detaills);
 
-                string who = $"{randomName} from {randomPlaece}";
-                string action = $"{randomAdvedb} {randomVerb} {randomNoun}";
+                if (randomNum % 2 == 0)
+                {
+                    string randomPlaece = GetRandomWord(plaeces);
+                    string randomVerb = GetRandomWord(verbs);
+                    string randomNoun = GetRandomWord(nouns);
 
-                string sentance = $"{who} {action} {randomDetail}.";
+                    string who = $"{randomName} from {randomPlaece}";
+                    string action = $"{randomAdvedb} {randomVerb} {randomNoun}";
 
-                Console.WriteLine(sentance);
+                    string declarativeSentence = $"{who} {action} {randomDetail}.";
+
+                    Console.WriteLine(declarativeSentence);
+                }
+                else
+                {
+                    string randomQuestionWord = GetRandomWord(questionWords);
+                    string randomHelpingVerb = GetRandomWord(helpingVerbs);
+                    string randomQuestionVerb = GetRandomWord(questionVerbs);
+
+                    string interrogativeSentence = $"{randomQuestionWord} {randomHelpingVerb} {randomName} {randomQuestionVerb} {randomAdvedb} {randomDetail}?";
+
+                    Console.WriteLine(interrogativeSentence);
+                }
+
                 Console.WriteLine("Click [Enter] to genrate new one.");
                 Console.WriteLine();
                 Console.ReadKey();

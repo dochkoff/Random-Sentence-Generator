e344cba708241ffe95d7e18e219c0e7da5145ed8
dochkoff <hounds.07cinder@icloud.com>
2022-12-17 18:14:15 +0200

    Update RandomSentenceGenerator.cs
    Added more names, nouns, verbs, adverbs, places and details

--- RandomSentenceGenerator.cs
+++ RandomSentenceGenerator.cs
@@ -6,12 +6,12 @@ namespace RandomSentenceGenerator
     {
         static void Main(string[] args)
         {
-            string[] names = { "Peter", "Michell", "Jane", "Steve" };
-            string[] plaeces = { "Sofia", "Plovdiv", "Varna", "Burgas" };
-            string[] verbs = { "eats", "holds", "sees", "plays with", "brings" };
-            string[] nouns = { "stones", "cake", "apple", "laptop", "bikes" };
-            string[] adverbs = { "slowly", "diligently", "warmly", "sadly", "rapidly" };
-            string[] detaills = { "near the river", "at home", "in the park" };
+            string[] names = { "Peter", "Michell", "Jane", "Steve", "Dave", "Gilbert", "Jorge", "Dan", "Brian", "Liam", "Ethan", "Lewis", "Milton", "Glen", "Antonio", "Connor", "Hunter", "Alberto", "Carlos", "Paul", "Gerald", "Joey", "Johnny", "Scott", "Martin", "Tommy", "Ted", "Nick" };
+            string[] plaeces = { "Sofia", "Plovdiv", "Varna", "Burgas", "Stara Zagora", "Veliko Tarnovo", "Yambol", "Haskovo", "Balchik", "Blagoevgrad", "Ruse", "Vidin", "Karlovo", "Kazanluk", "Botevgrad", "Pazardzhik", "Asenovgrad", "Svishtov", "Gorna Oryahovitsa", "Pleven", "Sevlievo", "Gabrovo", "Tryavna", "Troyan", "Elena", "Aitos" };
+            string[] verbs = { "eats", "holds", "sees", "plays with", "brings", "builds", "burns", "catches", "draws", "dreams", "finds", "forgets", "freezes", "hides", "keeps", "loses", "puts", "sends", "shows", "takes" };
+            string[] nouns = { "stones", "cake", "apple", "laptop", "bikes", "pizza", "car", "fruits", "musical instruments", "animal", "baby", "bridge", "camera", "credit card", "computer", "dad", "facts", "fish", "girl", "glass", "grass", "hand", "head" };
+            string[] adverbs = { "slowly", "diligently", "warmly", "sadly", "rapidly", "abnormally", "accidentally", "actually", "almost", "always", "beautifully", "carefully", "closely", "correctly", "easily", "elegantly", "eventually", "exactly", "fast", "often", "only", "safely" };
+            string[] detaills = { "near the river", "at home", "in the park", "at work", "near the city", "near the village", "in front of the house", "in the basement", "on the roof", "in the building", "after everything that happened" };
 
 
             Console.WriteLine("Hello, this is your firs random-generated sentance:");
@@ -31,6 +31,7 @@ namespace RandomSentenceGenerator
 
                 Console.WriteLine(sentance);
                 Console.WriteLine("Click [Enter] to genrate new one.");
+                Console.WriteLine();
                 Console.ReadKey();
             }
         }
